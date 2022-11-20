a1587239772d4f14f5f244761a20ca4eef1efd3f
alicialowe <hohpascal@outlook.com>
2022-11-20 17:28:06 +0000

    done basic setup on SetupOptions, Countdown, and Enabled. browser tested, currently no errors. no functionality apart from back to home button, however

---
+++ frontend_heyu/src/components/Countdown.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {useState, useEffect} from "react";
+
+//this is going to need props from SetupOptions - minutes, repetitions, image
+function Countdown() {
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Countdown;
--- frontend_heyu/src/pages/SetupOptions.js
+++ frontend_heyu/src/pages/SetupOptions.js
@@ -1,12 +1,35 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import {useState} from "react";
 
+//import images HERE to hand as props 
 
+//needs to HAND DOWN props to Enabled - minutes, repetitions, image
 function SetupOptions() {
-
+    const [minutes, setMinutes] = useState(0);
+    const [repetitions, setRepetitions] = useState(0);
+    const [image, setImage] = useState({});
 
     return (
-        <div>
+        <div className="options-pg">
 
+            <h2 className="back-home">
+                <Link to="/">hey-heyU</Link>
+            </h2>
+
+            <div className="options-pg-contents">
+                <section>
+                    <h1>Options Page</h1>
+
+                    <button className="to-countdown-btn">
+                        <Link to="/enabled">
+                        That's great, let's go
+                        </Link>
+                    </button>
+                </section>
+                
+                <div className="option-img"></div>
+            </div>
         </div>
     )
 }
