c99944901a942d188841b0ebda8aa2886c55a254
alicialowe <hohpascal@outlook.com>
2022-11-27 09:43:36 +0000

    notification sending successfully on first nudge, and also subsequent nudges. no duplicate notifications coming through and the reset timer is now working - hallefrickenlujiah

--- frontend_heyu/src/components/NewCountdown.js
+++ frontend_heyu/src/components/NewCountdown.js
@@ -12,8 +12,6 @@ function NewCountdown({minutes, setMinutes, resetMins}) {
     let date = new Date();
     let rightNow = date.getTime();
     const [now, setNow] = useState(rightNow);
-
-    const [isRunning, setIsRunning] = useState()
     
     useEffect(() => {
         let interval = setInterval(() => {
@@ -21,57 +19,63 @@ function NewCountdown({minutes, setMinutes, resetMins}) {
             clearInterval(interval);
             //added in the else if to make the countdown stop when reaches 00:00
             if (seconds === 0) {
+                //CHANGED SECONDS FROM 59 to 10 FOR TESTING
                 //to handle going down in minutes and restarting seconds
                 if (minutes !== 0) {
-                    setSeconds(59);
+                    setSeconds(10);
                     setMinutes(minutes - 1);
                 } else if (minutes === 0) {
                     clearInterval(interval);
                     setButton(true);
                 } else {
                     setMinutes(minutes);
-                    setSeconds(59);
+                    setSeconds(10);
                 }
                 //then also just counting down seconds if they're not at zero
             } else {
                 setSeconds(seconds - 1);
             }
         }, 1000)
+
+        console.log('this is now ', now);
         
         function resets() {
             clearInterval(interval);
             setMinutes(0);
             setSeconds(0);
+            console.log('timeout ran')
         }
-        setTimeout(() => resets(), (now + 60000) - now);
+        setTimeout(() => resets(), (now + 10000) - now);
+        //this cleanup function is what makes the interval clear and essentially makes the timer 00:00 and notification come through, in sync
+        return () => {
+            clearInterval(interval);
+            clearTimeout()
+        }
 
-        return () => clearInterval(interval);
-
-        // setTimeout(() => clearInterval(interval), (now + 59999) - now);
         //setButton(false); now moved to goAgain function to stop notification being resent on click
     }, [seconds]);
 
-    
-
-    // setTimeout(() => setNow(date.getTime()), (now + 60000) - now)
-
+    //THIS ISN'T WORKING - is zeroing out and sending notification immediately
      //trigger by button click, resets the countdown for the nudge
     //needs to be -1 on mins otherwise does not show correctly
     function goAgain() {
-        let update = date.getTime();
-        setNow(update);
+        updateNow();
         setButton(false);
         setMinutes(resetMins - 1);
-        setSeconds(59);
- 
+        setSeconds(10);
     }
 
-    // function updateNow() {
-    //     let date = new Date();
-    //     let update = date.getTime();
-    //     setNow(update);
-    // }
+    function updateNow() {
+        let update = date.getTime();
+        setNow(update);
+    }
 
+    useEffect(() => {
+        if (button === true) {
+            updateNow();
+        }
+    }, [])
+   
 
     //to ensure timer display is in 00:00 format, else would get single digits
     const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
